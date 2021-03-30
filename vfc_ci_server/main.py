a1bb57fe1e6a34a6bab7002f0e1ab2982f8e714e
Aurélien Delval <a.delval87@gmail.com>
2021-03-30 13:45:20 +0200

    Improvements to template anc vfc_ci_serve wrapper
    The vfc_ci_serve wrapper now accepts arguments: in particular, it is now
    possible to specify a git repository (either locally or by URL) hosted
    on Github or Gitlab. When specified, the template will include links to
    the different commits (directly on the plots) and to the repo's main
    page.

--- vfc_ci_server/main.py
+++ vfc_ci_server/main.py
@@ -1,30 +1,35 @@
 # Look for and read all the run files in the current directory (ending with
 # .vfcrun.hd5), and lanch a Bokeh server for the visualization of this data.
 
-
 import os
 import json
 import datetime
+import sys
 
 import pandas as pd
 
 from bokeh.plotting import figure, show, curdoc
 from bokeh.resources import INLINE
 from bokeh.embed import components
-from bokeh.models import Select, ColumnDataSource, Panel, Tabs, HoverTool
+from bokeh.models import Select, ColumnDataSource, Panel, Tabs, HoverTool, OpenURL, TapTool
 
 from jinja2 import Environment, FileSystemLoader
 
 
-        # Helper functions
+################################################################################
+
+
+        # HELPER FUNCTIONS
 
 # From a timestamp, return the associated metadata as a Pandas serie
 def get_metadata(timestamp):
     return metadata.loc[timestamp]
 
+
+
 # From an array of timestamps, return an array of hashes/dates that can
-# be used as the x series of a bar/box plot, as well as the metadata associated
-# to this x series (for the tooltips)
+# be used as the x series of a bar/box plot, as well as the metadata (in a dict
+# of arrays) associated to this x series (for the tooltips)
 def gen_x_series(timestamps):
     x = []
     x_metadata = dict(
@@ -52,6 +57,8 @@ def gen_x_series(timestamps):
         x_metadata["message"].append(row_metadata["message"])
 
     return x, x_metadata
+
+
 
 # Update plots based on current_test/var/backend combination
 def update_plots():
@@ -86,7 +93,7 @@ def update_plots():
     )
 
 
-# Draw the different plots (on a blank figure)
+    # Draw the different plots (on a blank figure)
 
 def gen_boxplot(plot):
     # (based on : https://docs.bokeh.org/en/latest/docs/gallery/boxplot.html)
@@ -105,6 +112,10 @@ def gen_boxplot(plot):
         ("μ", "@mu")
     ])
     plot.add_tools(hover)
+
+    if git_repo_linked:
+        tap = TapTool(callback=OpenURL(url=commit_link))
+        plot.add_tools(tap)
 
     # Stems
     plot.segment(x0="x", y0="max", x1="x", y1="quantile75", source=source,
@@ -138,6 +149,10 @@ def gen_sigma_plot(plot):
     ])
     plot.add_tools(hover)
 
+    if git_repo_linked:
+        tap = TapTool(callback=OpenURL(url=commit_link))
+        plot.add_tools(tap)
+
     plot.vbar(x="x", top="sigma", source=source, width=0.5, color="#008000")
     plot.xgrid.grid_line_color = None
     plot.ygrid.grid_line_color = None
@@ -153,6 +168,10 @@ def gen_s10_plot(plot):
         ("s", "@s10")
     ])
     plot.add_tools(hover)
+
+    if git_repo_linked:
+        tap = TapTool(callback=OpenURL(url=commit_link))
+        plot.add_tools(tap)
 
     plot.vbar(x="x", top="s10", source=source, width=0.5, color="#008000")
     plot.xgrid.grid_line_color = None
@@ -170,6 +189,10 @@ def gen_s2_plot(plot):
     ])
     plot.add_tools(hover)
 
+    if git_repo_linked:
+        tap = TapTool(callback=OpenURL(url=commit_link))
+        plot.add_tools(tap)
+
     plot.vbar(x="x", top="s2", source=source, width=0.5, color="#008000")
     plot.xgrid.grid_line_color = None
     plot.ygrid.grid_line_color = None
@@ -178,7 +201,7 @@ def gen_s2_plot(plot):
 ################################################################################
 
 
-    # Read vfcrun files and generate data/metadata
+    # READ VFCRUN FILES
 
 run_files = [ f for f in os.listdir(".") if f.endswith(".vfcrun.hd5") ]
 
@@ -204,9 +227,79 @@ data = data.groupby(["test", "variable", "vfc_backend"]).agg(lambda x: list(x))
 ################################################################################
 
 
-    # Setup Bokeh server
+    # SETUP BOKEH SERVER
 
 curdoc().title = "Verificarlo Report"
+
+
+    # Look for the Git repository remote address
+    # (if a Git repo is specified, the webpage will contain hyperlinks to the
+    # repository and the different commits)
+
+git_repo_linked = False
+
+if len(sys.argv) == 3:
+    from urllib.parse import urlparse
+
+    method = sys.argv[1]
+    address = sys.argv[2]
+    url = ""
+
+
+    # Here, address is either the remote URL or the path to the local Git repo
+    # (depending on the method)
+
+    if method == "remote":
+        # We should directly have a Git URL
+        url = address
+
+    elif method == "directory":
+        # Fetch the remote URL from the local repo
+        from git import Repo
+        repo = Repo(address)
+        url = repo.remotes.origin.url
+
+    else:
+        raise ValueError("The specified method to get the Git repository is "\
+        "invalid. Are you calling Bokeh directly instead of using the "\
+        "Verificarlo wrapper ?")
+
+
+    # At this point, "url" should be set correctly, we can get the repo's
+    # URL and name
+
+    parsed_url = urlparse(url)
+
+    path = parsed_url.path.split("/")
+    if len(path) < 3:
+        raise ValueError("The found URL doesn't seem to be pointing to a Git "\
+        "repository (path is too short)")
+
+    repo_name = path[2]
+
+    curdoc().template_variables["repo_url"] = url
+    curdoc().template_variables["repo_name"] = repo_name
+
+
+    # We should have a "github.com" or a "gitlab.XXX" URL
+
+    if parsed_url.netloc == "github.com":
+        commit_link = "https://" + parsed_url.netloc + parsed_url.path + "/commit/@hash"
+        curdoc().template_variables["commit_link"] = commit_link
+        curdoc().template_variables["git_host"] = "GitHub"
+        git_repo_linked = True
+
+    elif "gitlab." in parsed_url.netloc:
+        commit_link = "https://" + parsed_url.netloc + parsed_url.path + "/-/commit/@hash"
+        curdoc().template_variables["git_host"] = "GitLab"
+        git_repo_linked = True
+
+    else:
+        raise ValueError("The found URL doesn't seem to be a GitHub or GitLab URL")
+
+
+curdoc().template_variables["git_repo_linked"] = git_repo_linked
+
 
 
     # Selection setup (initially, we select the first test/var/backend combination)
@@ -253,7 +346,8 @@ source = ColumnDataSource(data=dict(
 # Box plot
 boxplot = figure(
     name="boxplot", title="Variable distribution over runs",
-    plot_width=800, plot_height=300, x_range=x
+    plot_width=800, plot_height=330, x_range=x,
+    tools="pan, wheel_zoom, xwheel_zoom, ywheel_zoom, reset, save"
 )
 gen_boxplot(boxplot)
 curdoc().add_root(boxplot)
@@ -261,7 +355,8 @@ curdoc().add_root(boxplot)
 # Sigma plot (bar plot)
 sigma_plot = figure(
     name="sigma_plot", title="Standard deviation σ over runs",
-    plot_width=800, plot_height=300, x_range=x
+    plot_width=800, plot_height=330, x_range=x,
+    tools="pan, wheel_zoom, xwheel_zoom, ywheel_zoom, reset, save"
 )
 gen_sigma_plot(sigma_plot)
 curdoc().add_root(sigma_plot)
@@ -269,14 +364,16 @@ curdoc().add_root(sigma_plot)
 # s plot (bar plot with 2 tabs)
 s10_plot = figure(
     name="s10_plot", title="Significant digits s over runs",
-    plot_width=800, plot_height=300, x_range=x
+    plot_width=800, plot_height=330, x_range=x,
+    tools="pan, wheel_zoom, xwheel_zoom, ywheel_zoom, reset, save"
 )
 gen_s10_plot(s10_plot)
 s10_tab = Panel(child=s10_plot, title="Base 10")
 
 s2_plot = figure(
     name="s2_plot", title="Significant digits s over runs",
-    plot_width=800, plot_height=300, x_range=x
+    plot_width=800, plot_height=330, x_range=x,
+    tools="pan, wheel_zoom, xwheel_zoom, ywheel_zoom, reset, save"
 )
 
 s2_tab = Panel(child=s2_plot, title="Base 2")
